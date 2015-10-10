d850734a2fff8ba4a3a16b40eebaa122921b1af3
Fuck <wangfangzheng@wangfangzhengdemacbook-pro.local>
2015-10-10 23:31:46 +0800

    dadsads

--- HMWeibo/HMWeibo/Classes/Module(功能模块)/Discover/DiscoverTableViewController.swift
+++ HMWeibo/HMWeibo/Classes/Module(功能模块)/Discover/DiscoverTableViewController.swift
@@ -13,6 +13,9 @@ class DiscoverTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
